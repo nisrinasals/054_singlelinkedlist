0182dbc0d7a0d8d1894f19883c8ffee14eb7a446
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-26 15:15:01 +0700

    menuliskan jika nim sudah ada

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -25,5 +25,20 @@ void addNode(){
             cout << "NIM sudah ada" << endl;
             return;
         }
+        nodeBaru-> next = START;
+        START = nodeBaru;
+        return;
+    }
+    
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs)){
+        if (nim == current->noMhs){
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current
+        current = current->next;
     }
 }
