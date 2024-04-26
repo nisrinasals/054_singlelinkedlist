87ff96bea2eabcea2163589249943f3026554bc3
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-26 15:54:01 +0700

    menuliskan case 4

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -162,7 +162,9 @@ int main(){
             case 3:
                 traverse();
                 break;
-            
+            case 4:
+                searchData();
+                break;
         }
     }
 }
