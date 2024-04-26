e462ed8226a3d0f29dade8ed0db2529b7c532352
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-26 15:31:40 +0700

    void traverse selesai

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -93,6 +93,6 @@ void traverse(){
         while (currentNode != NULL){
             cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
             currentNode = currentNode->next;
-        
+        }
     }
 }
