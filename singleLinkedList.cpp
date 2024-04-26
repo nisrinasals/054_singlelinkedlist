309dfcbdd447b5933784ada30e9b2cd2af57c944
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-26 15:49:09 +0700

    menuliskan jika nim berhasil dihapus

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -147,6 +147,17 @@ int main(){
                     system("cls");
                     break;
                 }
+
+                int nim;
+                cout << "Masukkan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)){
+                    cout << "nim: " << nim << " berhasil dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+                else
+                    
         }
     }
 }
